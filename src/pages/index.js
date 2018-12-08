ac0a4dee36a275d2c2ade222c84e894c55bae4cc
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-12-07 21:24:40 -0800

    start designing home layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
-import React from "react"
+import React from 'react'
+import { Image } from 'cloudinary-react'
+import { container, header, title } from '../styles/home-styles'
 
-export default () => <div>Hello world!</div>
+const Home = () =>
+	<div style={container}>
+		<div style={header}>
+			<Image
+				cloudName='ziro'
+				width='45'
+				publicId='logo-round_kxn8sa'
+				version='1508000699'
+				format='png'
+				secure='true'
+		    />
+			<span style={title}>Mural de vagas</span>
+		</div>
+		<p>
+			Confira as oportunidades atualmente em aberto na Ziro.
+		</p>
+	</div>
+
+export default Home
---
+++ src/styles/home-styles.js
@@ -0,0 +1,18 @@
+export const
+
+container = {
+	maxWidth: '900px',
+	margin: '40px auto 80px',
+	padding: '0px 7%'
+},
+
+header = {
+	display: 'grid',
+	gridTemplateColumns: '70px 1fr',
+	alignItems: 'center'
+},
+
+title = {
+	fontSize: '26px',
+	fontWeight: '600'
+}
